135268ebac38ecd2202616aa4e06cd68f2ce4bdd
zeineldin <m.zeineldin@leverzein.me>
2018-09-23 22:07:39 +0200

    add the 2 appications

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('DevOps Area NodeJs App Is Working :) \n');
+  res.send('App  1 Is Working :) \n');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
