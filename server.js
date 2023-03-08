7b447738586e0f1fbc1f40c76c9d421ad0aa604b
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2023-03-08 20:51:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('welcome to devops:) \n');
+  res.send('Osama) \n');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
